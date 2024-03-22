5fe7d0f0fe477d4397df34d5acc40dd1c2073b2c
Mehmet <mehmet-acar-gs@hotmail.com>
2024-03-23 00:20:52 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} isimli müşteri eklendi", musteri.Adi, musteri.Soyadi);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} isimli müşteri silindi", musteri.Adi, musteri.Soyadi);
+        }
+
+        public void List(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri: {0} {1} ", musteri.Adi, musteri.Soyadi);
+            } 
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.Adi = "Mehmet";
+musteri1.Soyadi = "Acar";
+musteri1.Yasi = 25;
+
+Musteri musteri2 = new Musteri();
+musteri2.Id = 2;
+musteri2.Adi = "Ali";
+musteri2.Soyadi = "Yılmaz";
+musteri2.Yasi = 30;
+
+Musteri musteri3 = new Musteri();
+musteri3.Id = 3;
+musteri3.Adi = "Ahmet";
+musteri3.Soyadi = "Demir";
+musteri3.Yasi = 35;
+
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.Add(musteri1);
+Console.WriteLine("-----------");
+musteriManager.Add(musteri2);
+Console.WriteLine("-----------");
+musteriManager.Add(musteri3);
+Console.WriteLine("-----------");
+
+musteriManager.Delete(musteri3);
+Console.WriteLine("-----------");
+
+Console.WriteLine("Müşteri Listesi");
+Musteri[] musteriler = new Musteri[] { musteri1, musteri2};
+musteriManager.List(musteriler);
